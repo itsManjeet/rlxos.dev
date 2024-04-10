837816cef7278c5d0e0272d929829bcca7c2bf10
itsmanjeet <itsmanjeet1998@gmail.com>
2024-04-10 13:22:20 +0530

    fixed features

--- components/Features.tsx
+++ components/Features.tsx
@@ -42,11 +42,7 @@ export default function Features() {
           <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Benefits of using rlxos
           </p>
-          <p className="mt-6 text-lg leading-8 text-gray-600">
-            Quis tellus eget adipiscing convallis sit sit eget aliquet quis.
-            Suspendisse eget egestas a elementum pulvinar et feugiat blandit at.
-            In mi viverra elit nunc.
-          </p>
+          <p className="mt-6 text-lg leading-8 text-gray-600"></p>
         </div>
         <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
           <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
