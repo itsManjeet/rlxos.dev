986a811d3ac2cf1c27a463193b21908747c1f716
itsmanjeet <itsmanjeet1998@gmail.com>
2024-04-10 16:52:18 +0530

    updated hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,9 +7,9 @@ export default function Hero() {
       <div className="mx-auto max-w-2xl pt-28 sm:pt-32 lg:pt-28">
         <div className="hidden sm:mb-8 sm:flex sm:justify-center">
           <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-            Checkout latest release.{" "}
+            Checkout latest announcements.{" "}
             <a
-              href="https://github.com/itsManjeet/rlxos/commits/stable/"
+              href="https://twitter.com/rlxos_dev"
               className="font-semibold text-indigo-600"
             >
               <span className="absolute inset-0" aria-hidden="true" />
