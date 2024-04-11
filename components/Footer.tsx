8635f0eb3667f2b0f2a6d2b2477c5772e3b838d1
itsmanjeet <itsmanjeet1998@gmail.com>
2024-04-11 14:52:32 +0530

    fixed footer interface on dark

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,27 +1,29 @@
 export default function Footer() {
   return (
-    <footer className="border-blue-gray-50 mx-auto flex flex max-w-7xl flex-row flex-wrap items-center justify-center gap-x-12 gap-y-6 border-t py-6 text-center md:justify-between">
-      <p className="block text-sm font-semibold leading-6 text-gray-900">
-        © 2023 rlxos
-      </p>
-      <ul className="flex flex-wrap items-center gap-x-8 gap-y-2">
-        <li>
-          <a
-            href="https://raw.githubusercontent.com/itsManjeet/rlxos/preview/LICENSE"
-            className="block text-sm font-semibold leading-6 text-gray-900 transition-colors hover:text-blue-500 focus:text-blue-500"
-          >
-            License
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://github.com/itsManjeet/rlxos/fork"
-            className="block text-sm font-semibold leading-6 text-gray-900 transition-colors hover:text-blue-500 focus:text-blue-500"
-          >
-            Contribute
-          </a>
-        </li>
-      </ul>
-    </footer>
+    <div className={"bg-white"}>
+      <footer className="border-blue-gray-50 mx-auto flex flex max-w-7xl flex-row flex-wrap items-center justify-center gap-x-12 gap-y-6 border-t py-6 text-center md:justify-between">
+        <p className="block text-sm font-semibold leading-6 text-gray-900">
+          © 2023 rlxos
+        </p>
+        <ul className="flex flex-wrap items-center gap-x-8 gap-y-2">
+          <li>
+            <a
+              href="https://raw.githubusercontent.com/itsManjeet/rlxos/preview/LICENSE"
+              className="block text-sm font-semibold leading-6 text-gray-900 transition-colors hover:text-blue-500 focus:text-blue-500"
+            >
+              License
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/itsManjeet/rlxos/fork"
+              className="block text-sm font-semibold leading-6 text-gray-900 transition-colors hover:text-blue-500 focus:text-blue-500"
+            >
+              Contribute
+            </a>
+          </li>
+        </ul>
+      </footer>
+    </div>
   );
 }
